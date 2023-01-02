acdb68f482973fc3658e2301085a7ca5dfe03112
Hidayat Chandra <hidayatchandra08@gmail.com>
2023-01-02 23:26:34 +0800

    Add snake, food and score

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,6 @@
 import * as image from './image.js';
 import * as audio from './audio.js';
 
-// Get canvas element
-const snake = document.getElementById('snake');
-const ctx = snake.getContext('2d');
-
 // component
 const boxSize = 32;
 
@@ -20,9 +16,31 @@ const foodChar = {
 };
 
 let score = 0;
+let highScore = localStorage.getItem('highscore') || 0;
 
+if (score > highScore) localStorage.setItem('highscore', score);
 function draw() {
+  const snake = document.getElementById('snake');
+  const ctx = snake.getContext('2d');
+
   ctx.drawImage(image.ground, 0, 0);
+  snakeChar.forEach((snake, index) => {
+    ctx.fillStyle = index === 0 ? 'green' : 'white';
+    ctx.fillRect(snake.x, snake.y, boxSize, boxSize);
+
+    ctx.strokeStyle = 'red';
+    ctx.strokeRect(snake.x, snake.y, boxSize, boxSize);
+  });
+
+  ctx.drawImage(image.food, foodChar.x, foodChar.y);
+
+  ctx.fillStyle = 'white';
+  ctx.font = '30px helvetica';
+  ctx.fillText(score, 2 * boxSize, 1.6 * boxSize);
+
+  ctx.fillStyle = 'white';
+  ctx.font = '30px helvetica';
+  ctx.fillText(highScore, 4.8 * boxSize, 1.6 * boxSize);
 }
 
 const game = setInterval(draw, 100);
